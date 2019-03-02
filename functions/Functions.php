7dfb9a00704ad5eb95e310a5da9c4ff44c6db126
Barley <memeguigui@qq.com>
2019-03-02 11:24:25 +0800

    添加一个检测是否为手机号的公共方法

--- functions/Functions.php
+++ functions/Functions.php
@@ -343,3 +343,26 @@ class Functions
     }
 	
 }
+
+/**
+ * 检查手机格式，中国手机不带国家代码，国际手机号格式为：国家代码-手机号
+ * @param $mobile
+ * @return bool
+ */
+public static function check_mobile($mobile)
+{
+    if (preg_match('/(^(13\d|14\d|15\d|16\d|17\d|18\d|19\d)\d{8})$/', $mobile)) {
+        return true;
+    } else {
+        if (preg_match('/^\d{1,4}-\d{5,11}$/', $mobile)) {
+            if (preg_match('/^\d{1,4}-0+/', $mobile)) {
+                //不能以0开头
+                return false;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+}
